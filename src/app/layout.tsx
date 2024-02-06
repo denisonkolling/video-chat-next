39f04de2bb02e1ec92e22111116db0b7f793a11b
Denison Kolling <denisonkolling@gmail.com>
2024-02-06 19:58:06 -0300

    chore: add socket context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Rubik } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Rubik } from 'next/font/google';
+import './globals.css';
+import { SocketProvider } from '@/context/SocketContext';
 
-const rubik = Rubik({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Video Chat!",
-  description: "Connecting people...",
+	title: 'Video Chat!',
+	description: 'Connecting people...',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={rubik.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={rubik.className}>
+				<SocketProvider>{children}</SocketProvider>
+			</body>
+		</html>
+	);
 }
